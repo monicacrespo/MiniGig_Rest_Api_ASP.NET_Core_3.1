3faefffeb9320c0b3f2ccb07628538a29e3c7794
Monica Crespo <32198408+monicacrespo@users.noreply.github.com>
2021-01-02 17:14:20 +0000

    Update GigsController.cs

--- Controllers/GigsController.cs
+++ Controllers/GigsController.cs
@@ -46,7 +46,7 @@ namespace MiniGigApi.Controllers
         // To protect from overposting attacks, enable the specific properties you want to bind to, for
         // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
         [HttpPut("{id}")]
-        public async Task<IActionResult> PutGig(int id, Gig gig)
+        public async Task<IActionResult> Put(int id, Gig gig)
         {
             if (id != gig.GigId)
             {
